351014fc75d3f48931be4e22225c3e681d1dc877
Gaspar Tartari <gaspartartari@gmail.com>
2024-02-13 14:07:35 -0300

    ProductController Integration Tests V1

--- backend/src/test/java/com/learning/dscatalog/controllers/ProductControllerIT.java
+++ backend/src/test/java/com/learning/dscatalog/controllers/ProductControllerIT.java
@@ -1,7 +1,10 @@
 package com.learning.dscatalog.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.springframework.http.MediaType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,20 +23,43 @@ public class ProductControllerIT {
 
     private Long existingId;
     private Long nonExistingId;
-    private Long dependentId;
-    private Pageable pageable;
+    private Long countTotalProducts;
+    private Long countTotalProductsFromComputerCategory;
+
 
     @BeforeEach
     void setup() throws Exception{
-        existingId = 1L;
-        nonExistingId = 2L;
-        dependentId = 3L;
+       
+        existingId = 1l;
+        nonExistingId = 10000L;
+        countTotalProducts = 25L;
+        countTotalProductsFromComputerCategory = 23L;
     }
 
     @Test
-    public void findAllShouldReturnSortedPageWhenSortByName(){
+    public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
 
-        
+        ResultActions result = mockMvc.perform(get("/products?sort=name,asc")
+            .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+        result.andExpect(jsonPath("$.content").exists());
+        result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+        result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+    }
+
+    @Test
+    public void findAllShouldReturnPageWithProductsOfComputerCategoryWhenSortByCategoryParamIsComputer() throws Exception {
+
+        ResultActions result = mockMvc.perform(get("/products?category=computers")
+            .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.totalElements").value(countTotalProductsFromComputerCategory));
+        result.andExpect(jsonPath("$.content").exists());
+       
     }
 
 }
