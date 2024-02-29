ea0c6c0675823792e57ecb1311c1efd1613be85d
Gaspar Tartari <gaspartartari@gmail.com>
2024-02-28 23:06:50 -0300

    Email recover p2

---
+++ backend/src/main/java/com/learning/dscatalog/entities/PasswordRecover.java
@@ -0,0 +1,96 @@
+package com.learning.dscatalog.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_password_recover")
+public class PasswordRecover {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant tokenExpiration;
+
+    public PasswordRecover(){
+
+    }
+
+    public PasswordRecover(Long id, String email, String token, Instant tokenExpiration) {
+        this.id = id;
+        this.email = email;
+        this.token = token;
+        this.tokenExpiration = tokenExpiration;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Instant getTokenExpiration() {
+        return tokenExpiration;
+    }
+
+    public void setTokenExpiration(Instant tokenExpiration) {
+        this.tokenExpiration = tokenExpiration;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PasswordRecover other = (PasswordRecover) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
--- backend/src/main/java/com/learning/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/learning/dscatalog/services/AuthService.java
@@ -1,16 +1,58 @@
 package com.learning.dscatalog.services;
 
+import java.time.Instant;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.learning.dscatalog.DTO.EmailDTO;
-
-import jakarta.validation.Valid;
+import com.learning.dscatalog.entities.PasswordRecover;
+import com.learning.dscatalog.entities.User;
+import com.learning.dscatalog.repositories.PasswordRecoverRepository;
+import com.learning.dscatalog.repositories.UserRepository;
+import com.learning.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class AuthService {
 
-    public void createRecoverToken(@Valid EmailDTO dto) {
-        
-    } 
-    
+    @Value("${email.password-recover.token.minutes}")
+    private Long tokenMinutes;
+
+    @Value("${email.password-recover.uri}")
+    private String recoverUri;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordRecoverRepository passwordRecoverRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Transactional
+    public void createRecoverToken(EmailDTO dto) {
+
+        User user = userRepository.findByEmail(dto.getEmail());
+        if (user == null)
+            throw new ResourceNotFoundException("Email not found");
+
+        String token = UUID.randomUUID().toString();
+        PasswordRecover entity = new PasswordRecover();
+        entity.setEmail(dto.getEmail());
+        entity.setToken(token);
+        entity.setTokenExpiration(Instant.now().plusSeconds(tokenMinutes * 60));
+
+        entity = passwordRecoverRepository.save(entity);
+
+        String body = "Access the link below to create a new password \n\n" +
+                recoverUri + token + " Expires in " + tokenMinutes + " minutes";
+
+        emailService.sendEmail(dto.getEmail(), "Password Recovery", body);
+
+    }
+
 }
